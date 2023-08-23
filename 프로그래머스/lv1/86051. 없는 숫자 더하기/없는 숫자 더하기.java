6cb4195e75fa76b2874079316e6a9845d8fa8a0e
SungMin Lee <111367394+eellda@users.noreply.github.com>
2023-08-23 13:32:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int num = 45;
+        
+        for (int i : numbers) {
+            num -= i;
+        }
+        return num;
+    }
+}
